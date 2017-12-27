c518804ac1db4c9b04fc6796054a34208b30b73f
Mathieu Bourgeois <mathieuuu@gmail.com>
2017-12-27 03:01:59 +0100

    Initial Commit

---
+++ unes du jourTests/unes_du_jourTests.swift
@@ -0,0 +1,36 @@
+//
+//  unes_du_jourTests.swift
+//  unes du jourTests
+//
+//  Created by BOURGEOIS Mathieu on 27/12/2017.
+//  Copyright © 2017 BOURGEOIS Mathieu. All rights reserved.
+//
+
+import XCTest
+@testable import unes_du_jour
+
+class unes_du_jourTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
